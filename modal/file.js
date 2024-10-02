fdb46918a2babb7e65bdfd791c339f9b93ed7972
Jeff himself <162985697+jeff0proprio@users.noreply.github.com>
2024-10-02 11:24:29 -0300

    Add files via upload

---
+++ modal/file.js
@@ -0,0 +1,24 @@
+
+var modal = document.getElementById("Modal");
+
+var btn = document.getElementById("Botao");
+
+var span = document.getElementsByClassName("Close")[0];
+
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
